91be48642a6ce6761b170e74d9f5a731e90a85ad
Training_h2a.09.22 <Training_h2a.09.22@HDC2-D-6YBKXH2.dir.svc.accenture.com>
2020-02-03 15:34:07 +0530

    Pushing all unstaged changes

---
+++ src/test/java/RunTest/RunnerTest.java
@@ -0,0 +1,13 @@
+package RunTest;
+
+import org.junit.runner.RunWith;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber .class)
+@CucumberOptions(features="demo30Jan.feature",glue= {"stepReg"},tags= {"@FunctionalTest"},plugin="json:target/cucumber-report.json")
+public class RunnerTest 
+{
+}
